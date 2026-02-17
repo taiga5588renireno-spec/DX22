9461835b16c2c276e95b10efc9e122f39f3f7c19
taiga5588renireno-spec <taiga5588renireno@gmail.com>
2026-02-17 16:52:10 +0900

    Update Collision.h

--- DX22_Project/Collision.h
+++ DX22_Project/Collision.h
@@ -20,9 +20,26 @@ public:
     {
         bool isHit = false;
         DirectX::XMFLOAT3 dir = { 0.0f, 0.0f, 0.0f };
+        DirectX::XMFLOAT3 point;
+    };
+public:
+    //--- 当たり判定の形状 
+    struct Plane { // 面 
+        DirectX::XMFLOAT3   normal;
+        DirectX::XMFLOAT3 pos;
+    };
+
+    struct Ray { // レイ 
+        DirectX::XMFLOAT3 start;
+        DirectX::XMFLOAT3 direction;
+
     };
 
 public:
     static Result Hit(Box a, Box b);
     static Result Hit(Sphere a, Sphere b);
+    static Result Hit(Plane plane, Ray ray);
+
+
+
 };
